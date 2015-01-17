3d11e9c9c7083ac7bd21b427f65de5c5e50e6b7d
Saurabh Sood <saurabhsood91@gmail.com>
2015-01-17 18:26:20 +0530

    add menubar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,5 +3,14 @@
 mainwindow::mainwindow(QWidget *parent) :
     QMainWindow(parent)
 {
+    //Create UI
+    menuBar = new QMenuBar();
+    fileMenu = new QMenu(tr("File"));
+    menuBar->addMenu(fileMenu);
+    this->setMenuBar(menuBar);
+
+    //Set Window Title
+    this->setWindowTitle(tr("Photo Viewer"));
+
     this->show();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
+#include <QActionGroup>
+#include <QMenuBar>
+#include <QMenu>
 
 class mainwindow : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit mainwindow(QWidget *parent = 0);
+private:
+    QMenuBar *menuBar;
+    QMenu *fileMenu;
 
 signals:
 
